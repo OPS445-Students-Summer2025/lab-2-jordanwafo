95c314f6808283a1f8971ce760df65a1d462440f
jordan wafo <mjndollawafo@myseneca.ca>
2025-05-23 16:21:53 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: mark jordan ndolla wafo
+# Author ID: mjndolla-wafo
+# Date Created: 2025/05/21
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
