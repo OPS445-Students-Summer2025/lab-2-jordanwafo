5018ddfc0edbd526ea82a587819494466f474e33
jordan wafo <mjndollawafo@myseneca.ca>
2025-05-23 16:21:31 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: mark jordan ndolla wafo
+# Author ID: mjndolla-wafo
+# Date Created: 2025/05/21
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
